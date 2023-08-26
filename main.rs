3beba935143d51cc9c800c30f1c5dc6cba61693a
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-08-26 14:48:11 +0530

    made a programe that takes the input from the user and print the input

--- main.rs
+++ main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main(){
-    println!("Hellow world!");
+    //create a new instance of the standard input stream
+    let stdin = io::stdin();
+
+    //print a massage to prompt the user input
+    println!("Please enter something:");
+
+    //create a mutable string to store the user's input
+    let mut input = String::new();
+
+    //Read the user's input and store it in the string
+    stdin.read_line(&mut input).expect("Failed to read the li9ne");
+
+    //prinnt the user's input
+    println!("you entered: {}",input);
+    
 }
