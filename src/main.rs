58f724ecf4b16c3378448b068525bc29851a4517
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-10-16 22:20:29 +0530

    variables, and datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use std::io;
+fn main() {
+    // Scalar Types
+    let x: i32 = 42;
+    let y: u64 = 100;
+    let a: f32 = 3.14;
+    let b: f64 = 2.71828;
+    let is_rust_fun: bool = true;
+    let heart_emoji: char = '❤';
 
-fn main(){
-    //create a new instance of the standard input stream
-    let stdin = io::stdin();
+    // Compound Types
+    let person: (String, i32, bool) = ("Alice".to_string(), 30, true);
+    let numbers: [i32; 3] = [1, 2, 3];
 
-    //print a massage to prompt the user input
-    println!("Please enter something:");
+    // Printing variables to the terminal
+    println!("Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+    println!("Single Precision Float: {}", a);
+    println!("Double Precision Float: {}", b);
+    println!("Is Rust Fun? {}", is_rust_fun);
+    println!("Heart Emoji: {}", heart_emoji);
 
-    //create a mutable string to store the user's input
-    let mut input = String::new();
+    // Destructuring a tuple
+    let (name, age, is_happy) = person;
+    println!("Name: {}", name);
+    println!("Age: {}", age);
+    println!("Is Happy? {}", is_happy);
 
-    //Read the user's input and store it in the string
-    stdin.read_line(&mut input).expect("Failed to read the li9ne");
-
-    //prinnt the user's input
-    println!("you entered: {}",input);
-
+    // Accessing array elements
+    println!("Array Element 1: {}", numbers[0]);
+    println!("Array Element 2: {}", numbers[1]);
+    println!("Array Element 3: {}", numbers[2]);
 }
